d2309a8467c0ac979732715ce15e2a2b071a596d
unknown <nhussaini@luc.edu>
2020-06-14 09:51:30 -0700

    given a dynamic size for each flashcard using useRef Hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css';
 import axios from 'axios';
