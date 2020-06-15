aa76370b293d8d387835a385247999829d24869e
unknown <nhussaini@luc.edu>
2020-06-14 19:58:40 -0700

    added a form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css';
 import axios from 'axios';
@@ -6,6 +6,19 @@ import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashCards] = useState(SAMPLE_FLASHCARDS);
+  const [categories, setCategories] = useState([]);
+
+  const categoryEl = useRef();
+  const amountEl =useRef();
+  
+
+  useEffect(()=>{
+    axios
+    .get('https://opentdb.com/api_category.php')
+    .then(res=>{
+      setCategories(res.data.trivia_categories);
+    })
+  })
 
   useEffect(()=>{
     axios
@@ -36,11 +49,37 @@ function App() {
     return textArea.value;
   }
 
+  function handleSubmit(e){
+    e.preventDefault();
+  }
 
   return (
-    <div className="container">
-      <FlashcardList flashcards={flashcards}/>
-    </div>
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map(category =>{
+              return <option value={category.id} key={category.id}>{category.name}</option>
+            })}
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="amount">Number of Question</label>
+          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+        </div>
+
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+
+      </form>
+      <div className="container">
+        <FlashcardList flashcards={flashcards}/>
+      </div>
+    </>
+    
     
   );
 }
