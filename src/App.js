156f58c69e208f036b607b75b69e1026a3c5d99e
unknown <nhussaini@luc.edu>
2020-06-14 20:31:01 -0700

    used params for speicif number and category of questions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 
 function App() {
-  const [flashcards, setFlashCards] = useState(SAMPLE_FLASHCARDS);
+  const [flashcards, setFlashCards] = useState([]);
   const [categories, setCategories] = useState([]);
 
   const categoryEl = useRef();
@@ -21,8 +21,24 @@ function App() {
   })
 
   useEffect(()=>{
+    
+  },[])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
     axios
-    .get('https://opentdb.com/api.php?amount=10')
+    .get('https://opentdb.com/api.php',{
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value 
+      }
+    })
     .then(res =>{
       console.log(res.data);
       setFlashCards(res.data.results.map((questionItem, index)=>{
@@ -41,16 +57,6 @@ function App() {
         }
       }))
     })
-  },[])
-
-  function decodeString(str) {
-    const textArea = document.createElement('textarea');
-    textArea.innerHTML = str;
-    return textArea.value;
-  }
-
-  function handleSubmit(e){
-    e.preventDefault();
   }
 
   return (
@@ -85,30 +91,30 @@ function App() {
 }
 
 
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question : 'what is 2+2?',
-    answer: '4',
-    options: [
-      '2',
-      '3',
-      '4',
-      '5'
-    ]
-  },
-  {
-    id: 2,
-    question : 'Question 2?',
-    answer: 'answer',
-    options: [
-      'answer 1',
-      'answer 2',
-      'answer 3',
-      'answer 4'
-    ]
-  }
+// const SAMPLE_FLASHCARDS = [
+//   {
+//     id: 1,
+//     question : 'what is 2+2?',
+//     answer: '4',
+//     options: [
+//       '2',
+//       '3',
+//       '4',
+//       '5'
+//     ]
+//   },
+//   {
+//     id: 2,
+//     question : 'Question 2?',
+//     answer: 'answer',
+//     options: [
+//       'answer 1',
+//       'answer 2',
+//       'answer 3',
+//       'answer 4'
+//     ]
+//   }
 
-]
+// ]
 
 export default App;
